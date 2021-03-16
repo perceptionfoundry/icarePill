91c8af7f24d59a44912d2467d7d927136de2ce65
ShahRukh <perception.khi@gmail.com>
2021-03-16 20:50:09 +0500

    first commit _ launch screen

---
+++ iCarePill/iCarePill/iCarePillApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCarePillApp.swift
+//  iCarePill
+//
+//  Created by Syed ShahRukh Haider on 16/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct iCarePillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
